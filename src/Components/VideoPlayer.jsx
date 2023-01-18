db2dc8a4efab716d6947a38ee86fbd5318ef7bd5
cybraia <27krishna2002@gmail.com>
2023-01-18 18:26:17 +0530

    changes for video compoent

--- src/Components/VideoPlayer.jsx
+++ src/Components/VideoPlayer.jsx
@@ -30,22 +30,23 @@ const VideoPlayer = () => {
 
     return (
       <Grid container className={classes.gridContainer}>
-        {stream && (
+          {stream && (
           <Paper className={classes.paper}>
             <Grid item xs={12} md={6}>
               <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
               <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
             </Grid>
           </Paper>
-        )}
-        {callAccepted && !callEnded && (
+          )}
+          {callAccepted && !callEnded && (
           <Paper className={classes.paper}>
             <Grid item xs={12} md={6}>
               <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
               <video playsInline ref={userVideo} autoPlay className={classes.video} />
             </Grid>
-          </Paper>
-        )}
+            </Paper>
+          )}
+       
       </Grid>
     );
   };
